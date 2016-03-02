7f9f19d7435b17da4609227323152c5ff2ce4afb
jjo <jonathanjowen@gmail.com>
2016-03-01 19:23:35 -0700

    3. Changes to be committed: 	modified:   server.R 	new file:   storm_table.csv 	modified:   ui.R
    server.R now reads storm_table.csv into a data frame and returns
    statistics for selected state and weather event type.
    ui.R reports 5 outputs from server.R

--- server.R
+++ server.R
@@ -1,6 +1,11 @@
 library(shiny)
-
+dataStorm <- read.csv("storm_table.csv")
 
 shinyServer(function(input, output) {  
-  output$inputValue  <- renderPrint({input$myState})
+  rowStorm <- reactive({as.integer(dataStorm[dataStorm$STATE==input$myState & dataStorm$evType==input$myStorm, 1])})
+  output$nEvent   <- renderPrint({dataStorm[rowStorm(), 4]})
+  output$PropDmg  <- renderPrint({dataStorm[dataStorm$STATE==input$myState & dataStorm$evType==input$myStorm, 7]})
+  output$CropDmg  <- renderPrint({dataStorm[dataStorm$STATE==input$myState & dataStorm$evType==input$myStorm, 8]})
+  output$Injury     <- renderPrint({dataStorm[dataStorm$STATE==input$myState & dataStorm$evType==input$myStorm, 10]})
+  output$Fatality   <- renderPrint({dataStorm[dataStorm$STATE==input$myState & dataStorm$evType==input$myStorm, 12]})
   })
--- ui.R
+++ ui.R
@@ -5,15 +5,34 @@
 library(shiny)
 data(state)
 listState <- state.abb
+listStorm <- c("astronomical low tide", "avalanche", "blizzard", 
+               "coastal flood", "cold/wind chill", "debris flow", "dense fog", 
+               "dense smoke", "drought", "dust devil", "dust storm", 
+               "excessive heat", "extreme cold/wind chill", "flash flood", 
+               "flood", "frost/freeze", "funnel cloud", "freezing fog", "hail", 
+               "heat", "heavy rain", "heavy snow", "high surf", "high wind", 
+               "hurricane (typhoon)", "ice storm", "lake-effect snow",
+               "lakeshore flood", "lightning", "marine hail",
+               "marine high wind", "marine strong wind", 
+               "marine thunderstorm wind", "rip current", "seiche", 
+               "sleet", "storm surge/tide", "strong wind", "thunderstorm wind", 
+               "tornado", "tropical depression", "tropical storm", "tsunami",
+               "volcanic ash", "waterspout", "wildfire", "winter storm", 
+               "winter weather")
 
 shinyUI(pageWithSidebar(
   # Application title
   headerPanel("Weather Event Loss Prediction"),
   sidebarPanel(
-    selectInput("myState", "choose state", listState)
+    selectInput("myState", "choose state", listState),
+    selectInput("myStorm", "choose weather event", listStorm)
      ),
   mainPanel(
-    h4("you entered", verbatimTextOutput("inputValue")
+    h4("number of events", verbatimTextOutput("nEvent")),
+    h4("mean property damage", verbatimTextOutput("PropDmg")),
+    h4("mean crop damage", verbatimTextOutput("CropDmg")),
+    h4("mean injuries", verbatimTextOutput("Injury")),
+    h4("mean fatalities", verbatimTextOutput("Fatality"))
     )
   )
-))
+)
